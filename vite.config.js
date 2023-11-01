5b406327cb66c93c57aa4ec4707d71a93aac57a9
Mayank sharma <mayanksharmasav97@gmail.com>
2023-11-01 22:39:04 +0530

    updated title, updated vite config to serve static assets

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  publicDir: 'assets',
   resolve: {
     alias: {
       '~':  path.resolve(__dirname, './src') 
